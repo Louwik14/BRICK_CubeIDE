82e90771e03de752fdc63619d8900b24b2dd60fd
Louwik14 <loicdu14150@hotmail.fr>
2026-01-29 19:40:00 +0100

    stable

---
+++ BRICK6_CUBE_fonctionnel/Debug/App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.c 
+
+OBJS += \
+./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.o 
+
+C_DEPS += \
+./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/%.o App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/%.su App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/%.cyclo: ../App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/%.c App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_PWR_LDO_SUPPLY -DUSE_HAL_DRIVER -DSTM32H743xx -c -I../App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/Middlewares/ST/STM32_USB_Device_Library/Class/CompositeBuilder/Src" -I../App/Middlewares/ST/STM32_USB_Device_Library/Class/CompositeBuilder/Inc -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/usb_stack" -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/Middlewares/ST/STM32_USB_Device_Library/Class/MIDI/Inc" -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/Middlewares/ST/STM32_USB_Host_Library/Core/Inc" -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/Middlewares/ST/STM32_USB_Device_Library/Core/Inc" -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App/Middlewares/ST/STM32_USB_Device_Library/Class/MIDI" -I../Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/App" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-App-2f-Middlewares-2f-ST-2f-STM32_USB_Device_Library-2f-Class-2f-AUDIO-2f-Src
+
+clean-App-2f-Middlewares-2f-ST-2f-STM32_USB_Device_Library-2f-Class-2f-AUDIO-2f-Src:
+	-$(RM) ./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.cyclo ./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.d ./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.o ./App/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/usbd_audio.su
+
+.PHONY: clean-App-2f-Middlewares-2f-ST-2f-STM32_USB_Device_Library-2f-Class-2f-AUDIO-2f-Src
+
