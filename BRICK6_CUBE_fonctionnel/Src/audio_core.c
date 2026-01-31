a90af74a92ad98f8457462e38f1015cb0bd24ff5
Louwik14 <loicdu14150@hotmail.fr>
2026-01-31 18:46:22 +0100

    Add passive audio_core RX copy hook

---
+++ BRICK6_CUBE_fonctionnel/Src/audio_core.c
@@ -0,0 +1,24 @@
+#include "audio_core.h"
+
+#include <string.h>
+
+static int32_t core_input_copy[AUDIO_CORE_FRAMES_PER_BLOCK * AUDIO_CORE_CHANNELS];
+
+void audio_core_init(void) {
+}
+
+void audio_core_process_block(void) {
+}
+
+void audio_core_on_input_block(const int32_t *data, uint32_t frames) {
+  if (data == NULL) {
+    return;
+  }
+
+  if (frames > AUDIO_CORE_FRAMES_PER_BLOCK) {
+    frames = AUDIO_CORE_FRAMES_PER_BLOCK;
+  }
+
+  size_t sample_count = (size_t)frames * AUDIO_CORE_CHANNELS;
+  memcpy(core_input_copy, data, sample_count * sizeof(int32_t));
+}
