6a1bd11820561ab6180a0d7b07852cb1e6763c2f
Louwik14 <loicdu14150@hotmail.fr>
2026-03-05 08:31:55 +0100

    Fix SD DMA completion callbacks for FatFs mount

--- BRICK6_CUBE_fonctionnel/Src/bsp_driver_sd.c
+++ BRICK6_CUBE_fonctionnel/Src/bsp_driver_sd.c
@@ -233,6 +233,26 @@ void HAL_SD_AbortCallback(SD_HandleTypeDef *hsd)
   BSP_SD_AbortCallback();
 }
 
+/**
+  * @brief Tx Transfer completed callback
+  * @param hsd: SD handle
+  * @retval None
+  */
+void HAL_SD_TxCpltCallback(SD_HandleTypeDef *hsd)
+{
+  BSP_SD_WriteCpltCallback();
+}
+
+/**
+  * @brief Rx Transfer completed callback
+  * @param hsd: SD handle
+  * @retval None
+  */
+void HAL_SD_RxCpltCallback(SD_HandleTypeDef *hsd)
+{
+  BSP_SD_ReadCpltCallback();
+}
+
 /* USER CODE BEGIN CallBacksSection_C */
 /**
   * @brief BSP SD Abort callback
