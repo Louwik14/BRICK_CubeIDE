f678392e2fa74832bf55f5b02b9896040a442c41
Louwik14 <loicdu14150@hotmail.fr>
2026-03-15 12:00:31 +0100

    Add dedicated potentiometer mux driver and integrate scan task

---
+++ BRICK6_CUBE_fonctionnel/Inc/App/mux_pots.h
@@ -0,0 +1,18 @@
+#ifndef APP_MUX_POTS_H
+#define APP_MUX_POTS_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mux_pots_init(void);
+void mux_pots_scan(void);
+uint16_t mux_pots_get(uint8_t pot);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_MUX_POTS_H */
--- BRICK6_CUBE_fonctionnel/Src/Core/engine_tasklet.c
+++ BRICK6_CUBE_fonctionnel/Src/Core/engine_tasklet.c
@@ -35,6 +35,7 @@
 #include "buttons.h"
 #include "encoders.h"
 #include "led_rgb.h"
+#include "mux_pots.h"
 
 volatile uint32_t engine_tick_count = 0U;
 
@@ -72,6 +73,7 @@ static void engine_tick(uint32_t dt_ms)
   buttons_update(dt_ms);
   encoders_update(dt_ms);
   led_service(dt_ms);
+  mux_pots_scan();
 }
 
 /* ============================================================
@@ -108,6 +110,7 @@ void engine_tasklet_init(uint32_t sample_rate)
 
   buttons_init();
   encoders_init();
+  mux_pots_init();
 }
 
 /* ============================================================
