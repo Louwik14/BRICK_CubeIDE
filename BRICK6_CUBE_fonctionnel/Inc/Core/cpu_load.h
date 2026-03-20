00a053480a721565c7308281583feb136eb44011
Louwik14 <loicdu14150@hotmail.fr>
2026-03-20 14:12:23 +0100

    Clamp CPU load peak to 100 percent

--- BRICK6_CUBE_fonctionnel/Inc/Core/cpu_load.h
+++ BRICK6_CUBE_fonctionnel/Inc/Core/cpu_load.h
@@ -2,6 +2,19 @@
 
 #include <stdint.h>
 
+typedef struct
+{
+    uint32_t last_permille;
+    uint32_t avg_permille;
+    uint32_t peak_permille;
+    uint32_t peak_recent_permille;
+    uint32_t over_80_count;
+    uint32_t over_90_count;
+    uint32_t over_100_count;
+    uint32_t block_count;
+    uint32_t counter_valid;
+} cpu_load_metrics_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -23,7 +36,16 @@ void cpu_load_irq_end(void);
 
 uint32_t cpu_load_get_permille(void);
 uint32_t cpu_load_get_max(void);
+uint32_t cpu_load_get_avg_permille(void);
+uint32_t cpu_load_get_peak_recent_permille(void);
+uint32_t cpu_load_get_over_80_count(void);
+uint32_t cpu_load_get_over_90_count(void);
+uint32_t cpu_load_get_over_100_count(void);
+uint32_t cpu_load_get_block_count(void);
 uint32_t cpu_load_is_valid(void);
+
+void cpu_load_get_metrics(cpu_load_metrics_t *metrics);
+void cpu_load_reset_peak(void);
 
 #ifdef __cplusplus
 }
