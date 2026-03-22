983ef7eef3bb356e8a3bcdae9cba04d04b284079
Louwik14 <loicdu14150@hotmail.fr>
2026-03-22 21:37:41 +0100

    ui: add filter template family

---
+++ BRICK6_CUBE_fonctionnel/Inc/UI/pages/ui_page_template_filter.h
@@ -0,0 +1,8 @@
+#ifndef UI_PAGE_TEMPLATE_FILTER_H
+#define UI_PAGE_TEMPLATE_FILTER_H
+
+#include "ui_page.h"
+
+extern const ui_page_t g_ui_page_template_filter;
+
+#endif /* UI_PAGE_TEMPLATE_FILTER_H */
