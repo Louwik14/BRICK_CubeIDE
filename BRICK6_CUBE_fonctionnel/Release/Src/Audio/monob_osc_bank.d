4e52b1befa152de796794ec9ce384e73cfb4983e
Louwik14 <loicdu14150@hotmail.fr>
2026-03-23 16:14:15 +0100

    add docs old kb

---
+++ BRICK6_CUBE_fonctionnel/Release/Src/Audio/monob_osc_bank.d
@@ -0,0 +1,6 @@
+Src/Audio/monob_osc_bank.o: ../Src/Audio/monob_osc_bank.cpp \
+ ../Inc/Audio/monob_osc_bank.h ../Inc/Audio/oscillator.h \
+ C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/Drivers/Daisy_SP/dsp.h
+../Inc/Audio/monob_osc_bank.h:
+../Inc/Audio/oscillator.h:
+C:/Users/developpeur/Documents/BRICK5_H743_176/BRICK6_CUBE_fonctionnel/Drivers/Daisy_SP/dsp.h:
--- BRICK6_CUBE_fonctionnel/Release/Src/Audio/monob_synth.d
+++ BRICK6_CUBE_fonctionnel/Release/Src/Audio/monob_synth.d
@@ -1,4 +1,6 @@
 Src/Audio/monob_synth.o: ../Src/Audio/monob_synth.c \
- ../Inc/Audio/monob_synth.h ../Inc/Audio/fx_filter_ladder_moog.h
+ ../Inc/Audio/monob_synth.h ../Inc/Audio/fx_filter_ladder_moog.h \
+ ../Inc/Audio/monob_osc_bank.h
 ../Inc/Audio/monob_synth.h:
 ../Inc/Audio/fx_filter_ladder_moog.h:
+../Inc/Audio/monob_osc_bank.h:
