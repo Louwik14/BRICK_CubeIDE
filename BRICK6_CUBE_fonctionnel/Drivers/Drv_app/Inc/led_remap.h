2e830be6c823f28c027747db82f9663c0b786cd2
Louwik14 <loicdu14150@hotmail.fr>
2026-03-23 21:47:07 +0100

    Fix hall LED remap application

--- BRICK6_CUBE_fonctionnel/Drivers/Drv_app/Inc/led_remap.h
+++ BRICK6_CUBE_fonctionnel/Drivers/Drv_app/Inc/led_remap.h
@@ -55,22 +55,22 @@ static const uint8_t g_led_remap_hall_for_led[HALL_KEY_COUNT] = {
  * can map a logical hall back to its real LED without recomputing anything.
  */
 static const led_id_t g_led_remap_led_for_hall[HALL_KEY_COUNT] = {
-    [0]  = LED_STEP_9,
-    [1]  = LED_STEP_8,
-    [2]  = LED_STEP_7,
+    [0]  = LED_STEP_5,
+    [1]  = LED_STEP_7,
+    [2]  = LED_STEP_8,
     [3]  = LED_STEP_6,
-    [4]  = LED_STEP_5,
-    [5]  = LED_STEP_4,
-    [6]  = LED_STEP_3,
-    [7]  = LED_STEP_2,
-    [8]  = LED_STEP_1,
-    [9]  = LED_STEP_16,
-    [10] = LED_STEP_15,
+    [4]  = LED_STEP_4,
+    [5]  = LED_STEP_1,
+    [6]  = LED_STEP_2,
+    [7]  = LED_STEP_3,
+    [8]  = LED_STEP_13,
+    [9]  = LED_STEP_15,
+    [10] = LED_STEP_16,
     [11] = LED_STEP_14,
-    [12] = LED_STEP_13,
-    [13] = LED_STEP_12,
-    [14] = LED_STEP_11,
-    [15] = LED_STEP_10,
+    [12] = LED_STEP_12,
+    [13] = LED_STEP_9,
+    [14] = LED_STEP_10,
+    [15] = LED_STEP_11,
 };
 
 static inline bool led_remap_is_param_led(led_id_t led)
