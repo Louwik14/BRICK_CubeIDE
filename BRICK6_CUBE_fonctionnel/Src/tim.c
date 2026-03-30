526184d08f084401339c72cb436cbaeafc44e05c
Louwik14 <loicdu14150@hotmail.fr>
2026-03-30 17:10:42 +0200

    seq: align internal logical time base with TIM5 cadence

--- BRICK6_CUBE_fonctionnel/Src/tim.c
+++ BRICK6_CUBE_fonctionnel/Src/tim.c
@@ -132,6 +132,11 @@ void MX_TIM5_Init(void)
   {
     Error_Handler();
   }
+  sConfigOC.Pulse = 666;
+  if (HAL_TIM_OC_ConfigChannel(&htim5, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN TIM5_Init 2 */
 
   /* USER CODE END TIM5_Init 2 */
