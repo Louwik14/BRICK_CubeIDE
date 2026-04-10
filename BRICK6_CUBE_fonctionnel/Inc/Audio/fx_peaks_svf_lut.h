6f6399f3634c2d79573745c8f000862a617b8bee
Louwik14 <loicdu14150@hotmail.fr>
2026-04-10 23:23:14 +0200

    Replace runtime biquad filter with Peaks SVF core

---
+++ BRICK6_CUBE_fonctionnel/Inc/Audio/fx_peaks_svf_lut.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const uint16_t fx_peaks_lut_svf_cutoff[257];
+extern const uint16_t fx_peaks_lut_svf_damp[257];
+extern const uint16_t fx_peaks_lut_svf_scale[257];
+
+#ifdef __cplusplus
+}
+#endif
