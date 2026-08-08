9ef0d722d228f77e9f0919fd39dce238c38bb135
Louwik14 <loicdu14150@hotmail.fr>
2026-08-08 14:26:44 +0200

    planb: require explicit product variant

--- Inc/Core/brick_build_config.h
+++ Inc/Core/brick_build_config.h
@@ -9,11 +9,17 @@
 #error "BRICK_TEST_BUILD must be defined to 0 or 1"
 #endif
 
+#if defined(BRICK6_VARIANT_LOWCOST) && defined(BRICK6_VARIANT_PREMIUM)
+#error "BRICK6_VARIANT_LOWCOST and BRICK6_VARIANT_PREMIUM are mutually exclusive"
+#elif !defined(BRICK6_VARIANT_LOWCOST) && !defined(BRICK6_VARIANT_PREMIUM)
+#error "A supported BRICK6 variant must be selected"
+#endif
+
 #if defined(BRICK6_VARIANT_LOWCOST)
 #define BRICK6_LOOPER_GLOBAL_CAP 1U
 #define BRICK6_LOOPER_RAW_SLOT_CAP 1U
 #define BRICK6_LOOPER_SHIFTER_SLOT_CAP 1U
-#else
+#elif defined(BRICK6_VARIANT_PREMIUM)
 #define BRICK6_LOOPER_GLOBAL_CAP 8U
 #define BRICK6_LOOPER_RAW_SLOT_CAP 4U
 #define BRICK6_LOOPER_SHIFTER_SLOT_CAP 4U
